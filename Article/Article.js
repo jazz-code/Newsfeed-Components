4be13663db644a6a464be1c358a068ab7fd3d9a7
jazz-code <ren.meta@gmail.com>
2019-06-19 15:36:05 -0400

    expand added to html / finished article.js instructions

--- Article/Article.js
+++ Article/Article.js
@@ -4,18 +4,19 @@
 class Article {
   constructor(article) {
     // assign this.domElement to the passed in domElement
-    this.article; 
+    this.article = article;
     // create a reference to the ".expandButton" class. 
-    this.expandButton;
+    this.expandButton = this.article.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    
+    // Added "Expand" into HTML
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    
+    this.expandButton.addEventListener('click', () => this.expandArticle());
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-
+    this.article.classList.toggle('close');
+    this.article.classList.toggle('article-open');
   }
 }
 
@@ -29,8 +30,9 @@ class Article {
 
 */
 
-let articles = document.querySelectorAll('.article');
+const articles = document.querySelectorAll('.article');
 
-articles.forEach((article) => new Article(article));
+articles.forEach(function(article) {
+  return new Article(article);
+})
 
-console.log(articles)
