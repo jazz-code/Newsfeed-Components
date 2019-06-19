cb55e9237752dd5ff6792a0ab506fa73b099a1ac
jazz-code <ren.meta@gmail.com>
2019-06-19 17:45:30 -0400

    stretch -- added button close functionality WIP

--- Article/Article.js
+++ Article/Article.js
@@ -6,17 +6,20 @@ class Article {
     // assign this.domElement to the passed in domElement
     this.article = article;
     // create a reference to the ".expandButton" class. 
+    this.buttons = this.article.querySelector('.buttons')
     this.expandButton = this.article.querySelector('.expandButton');
+    this.closeButton = this.article.querySelector('.closeButton')
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     // Added "Expand" into HTML
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    this.expandButton.addEventListener('click', () => this.expandArticle());
+    this.buttons.addEventListener('click', () => this.expandArticle());
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
     this.article.classList.toggle('close');
     this.article.classList.toggle('article-open');
+    this.article.classList.toggle('closeButton')
   }
 }
 
